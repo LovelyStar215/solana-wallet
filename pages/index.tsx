aa1d20f9e3657ea2504c86baa7d4d4d59a0bfbcf
LovelyStar215 <ericjin106@gmail.com>
2021-09-24 15:18:23 -0500

    Import antd and styled-components; add Wrapper component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { Row } from "antd";
+import styled from "styled-components";
 
 const Home: NextPage = () => {
   return (
@@ -16,10 +17,16 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        
+
+        <Wrapper>Hello</Wrapper>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+const Wrapper = styled(Row)`
+  margin: 0 auto;
+  width: 500px;
+`;
+
+export default Home;
