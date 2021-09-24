29fe3ea5cdbeef26094ca07486610ec90f804280
LovelyStar215 <ericjin106@gmail.com>
2021-09-24 17:08:07 -0500

    Add header elements to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,20 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { Row } from "antd";
-import styled from "styled-components";
+import { Menu, Dropdown } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 
 const Home: NextPage = () => {
+  const onChange = () => {};
+
+  const menu = (
+    <Menu>
+      <Menu.Item>Mainnet</Menu.Item>
+      <Menu.Item>Devnet</Menu.Item>
+      <Menu.Item>Testnet</Menu.Item>
+    </Menu>
+  );
+
   return (
     <div className={styles.container}>
       <Head>
@@ -14,19 +24,24 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <header className={styles.header}>
+          <div className={`${styles.top} ${styles.logo}`}>MyWallet</div>
+          <Dropdown className={styles.top} overlay={menu}>
+            <a
+              className="ant-dropdown-link"
+              onClick={(e) => e.preventDefault()}
+            >
+              Network <DownOutlined />
+            </a>
+          </Dropdown>
+        </header>
+
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
-        <Wrapper>Hello</Wrapper>
       </main>
     </div>
   );
 };
-
-const Wrapper = styled(Row)`
-  margin: 0 auto;
-  width: 500px;
-`;
 
 export default Home;
