1cc7551101c1c0f3f3b96de85eb6064580aa9cff
LovelyStar215 <ericjin106@gmail.com>
2021-09-27 11:56:52 -0500

    Move antd css import above globals.css import to be able to override antd defaults

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
+import 'antd/dist/antd.css';
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import 'antd/dist/antd.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
