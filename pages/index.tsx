85e01ad23bed17a9d60b8495e1b4326e2ef8e1f9
LovelyStar215 <ericjin106@gmail.com>
2021-09-28 09:39:28 -0500

    Remove layout elements from index

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,77 +6,22 @@ import { DownOutlined } from "@ant-design/icons";
 import CreateAccount from "../components/CreateAccount";
 import RestoreAccount from "../components/RestoreAccount";
 import React, { useState } from "react";
+import withPublicLayout from "../components/Layout/withPublicLayout";
 
 const Home: NextPage = () => {
-  const [network, setNetwork] = useState<string>("Devnet");
-
-  // What type should e be here?
-  // Also check console warning
-  const selectNetwork = (e: any) => {
-    const text = e.domEvent.target.innerText.trim();
-    setNetwork(text);
-  };
-
-  const menu = (
-    <Menu>
-      <Menu.Item onClick={selectNetwork}>
-        Mainnet {network === "Mainnet" && <Badge status="processing" />}
-      </Menu.Item>
-      <Menu.Item onClick={selectNetwork}>
-        Devnet {network === "Devnet" && <Badge status="processing" />}
-      </Menu.Item>
-      <Menu.Item onClick={selectNetwork}>
-        Testnet {network === "Testnet" && <Badge status="processing" />}
-      </Menu.Item>
-    </Menu>
-  );
-
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Wallet Tutorial</title>
-        <meta
-          name="description"
-          content="Solana wallet tutorial by Figment Learn"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <h1 className={styles.title}>
+        A simple, non-custodial crypto wallet for managing{" "}
+        <a href="https://solana.com/">Solana</a> digital assets.
+      </h1>
 
-      <main className={styles.main}>
-        {/* Consider abstracting into separate header component */}
-        <header className={styles.header}>
-          <div className={`${styles.top} ${styles.logo}`}>MyWallet</div>
-          <Dropdown className={styles.top} overlay={menu}>
-            <a
-              className="ant-dropdown-link"
-              onClick={(e) => e.preventDefault()}
-            >
-              Network <DownOutlined />
-            </a>
-          </Dropdown>
-        </header>
-
-        <h1 className={styles.title}>
-          A simple, non-custodial crypto wallet for managing{" "}
-          <a href="https://solana.com/">Solana</a> digital assets.
-        </h1>
-
-        <div className={styles.grid}>
-          <CreateAccount></CreateAccount>
-          <RestoreAccount></RestoreAccount>
-        </div>
-
-        <footer className={styles.footerHome}>
-          <p>
-            MyWallet tutorial created by{" "}
-            <a className={styles.footerLink} href="https://learn.figment.io/">
-              Figment Learn
-            </a>
-          </p>
-        </footer>
-      </main>
-    </div>
-  );
+      <div className={styles.grid}>
+        <CreateAccount></CreateAccount>
+        <RestoreAccount></RestoreAccount>
+      </div>
+    </>
+  )
 };
 
-export default Home;
+export default withPublicLayout(Home);
