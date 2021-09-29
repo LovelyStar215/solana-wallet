f49c069b7ff2ff42ef0b60efd211905db4f03ba0
LovelyStar215 <ericjin106@gmail.com>
2021-09-29 15:23:31 -0500

    Link import button to /recover

--- components/RestoreAccount/index.tsx
+++ components/RestoreAccount/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "../../styles/HomeBoxes.module.css";
 import { Button } from "antd";
 import { DownloadOutlined, UnlockOutlined } from "@ant-design/icons";
+import Link from "next/link";
 
 const RestoreAccount = () => {
   return (
@@ -11,14 +12,16 @@ const RestoreAccount = () => {
         <UnlockOutlined
           style={{ fontSize: "3rem", margin: "2rem 0", display: "block" }}
         />
-        <h2>Import Wallet!</h2>
+        <h2>Import Wallet</h2>
         <p>
           Use your secret recovery phrase to restore an existing Solana wallet.
         </p>
       </header>
 
       <div className={styles.buttons}>
-        <Button>Get Existing Wallet</Button>
+        <Link href={`/recover`} passHref>
+          <Button>Get Existing Wallet</Button>
+        </Link>
       </div>
     </div>
   );
