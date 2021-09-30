8b05d53565ae487550573ba91529ac13a957d4ec
LovelyStar215 <ericjin106@gmail.com>
2021-09-30 17:55:44 -0500

    Create wallet outline

--- pages/wallet.tsx
+++ pages/wallet.tsx
@@ -1,8 +1,17 @@
 import { NextPage } from "next";
 import withPublicLayout from "../components/Layout/withPublicLayout";
+import { Button } from "antd";
+import { ToTopOutlined } from "@ant-design/icons";
 
 const Wallet: NextPage = () => {
-  return <div>Wallet</div>;
+  return (
+    <>
+      <h1>Account Dashboard</h1>
+      <p>Account: adasdasasdasdasdsdsas</p>
+      <h2>0 <span>SOL</span></h2>
+      <Button type="primary">Send <ToTopOutlined /></Button>
+    </>
+  );
 };
 
 export default withPublicLayout(Wallet);
