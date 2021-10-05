49cdbbb9975f05f5e1b3bd07b5e0b4259f95d075
LovelyStar215 <ericjin106@gmail.com>
2021-10-05 14:57:57 -0500

    Add loading state to generate wallet

--- pages/generate.tsx
+++ pages/generate.tsx
@@ -1,18 +1,32 @@
+import React, { useState } from "react";
 import { NextPage } from "next";
 import styles from "../styles/Phrase.module.css";
-import { Button } from "antd";
+import { Button, Spin } from "antd";
 import Link from "next/link";
+import { LoadingOutlined } from "@ant-design/icons";
 
 const Phrase: NextPage = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleLoading = () => {
+    setLoading(true)
+  };
+
   return (
     <>
       <h1 className={styles.title}>Create New Wallet</h1>
 
       <p>Generate a key phrase to setup your Solana wallet.</p>
 
-      <Link href={`/phrase`} passHref>
-        <Button type="default">Generate</Button>
-      </Link>
+      {!loading && (
+        <Link href={`/phrase`} passHref>
+          <Button type="default" onClick={handleLoading}>
+            Generate
+          </Button>
+        </Link>
+      )}
+
+      {loading && <LoadingOutlined style={{ fontSize: 24 }} spin />}
     </>
   );
 };
