801058c917327b87e8e72db899defdc6507b0702
LovelyStar215 <ericjin106@gmail.com>
2021-10-06 15:50:10 -0500

    Make transaction amount dynamic, add loading state to send button, change check image

--- components/TransactionLayout/index.tsx
+++ components/TransactionLayout/index.tsx
@@ -1,9 +1,18 @@
 import React, { useState, useContext } from "react";
 import { Input, Button } from "antd";
 import { GlobalContext } from "../../context";
-import { Connection, clusterApiUrl, LAMPORTS_PER_SOL, SystemProgram, Transaction, sendAndConfirmTransaction, PublicKey } from "@solana/web3.js";
-import styled from 'styled-components';
-import Image from 'next/image'
+import {
+  Connection,
+  clusterApiUrl,
+  LAMPORTS_PER_SOL,
+  SystemProgram,
+  Transaction,
+  sendAndConfirmTransaction,
+  PublicKey,
+} from "@solana/web3.js";
+import styled from "styled-components";
+const converter = require("number-to-words");
+import { LoadingOutlined } from "@ant-design/icons";
 
 // TODOS
 // - add date
@@ -17,44 +26,40 @@ import Image from 'next/image'
 // - handle insufficient funds error
 
 type FormT = {
-  from: string
-  to: string
-  amount: number
-  isSigned: boolean
-}
+  from: string;
+  to: string;
+  amount: number;
+  isSigned: boolean;
+};
 
 const defaultForm: FormT = {
-  from: '',
-  to: '',
+  from: "",
+  to: "",
   amount: 0,
   isSigned: false,
-}
+};
 
 const TransactionModal = () => {
   const { network, account } = useContext(GlobalContext);
   const [form, setForm] = useState<FormT>(defaultForm);
   const [sending, setSending] = useState<boolean>(false);
-  
+
   const onFieldChange = (field, value) => {
     setForm({
       ...form,
-      [field]: value
-    })
-  }
+      [field]: value,
+    });
+  };
 
   const transfer = async () => {
-    if (!account) return
+    if (!account) return;
     const connection = new Connection(clusterApiUrl(network), "confirmed");
-
-    console.log(form)
 
     const instructions = SystemProgram.transfer({
       fromPubkey: account.publicKey,
       toPubkey: new PublicKey(form.to),
-      lamports: 10000,
+      lamports: form.amount,
     });
-
-    console.log('instructions', instructions)
 
     const signers = [
       {
@@ -63,29 +68,54 @@ const TransactionModal = () => {
       },
     ];
 
-    console.log('signers', signers)
-
     const transaction = new Transaction().add(instructions);
 
-    console.log('transaction', transaction)
+    setSending(true);
 
-    setSending(true)
     const hash = await sendAndConfirmTransaction(
       connection,
       transaction,
-      signers,
+      signers
     );
-    setSending(false)
-    console.log('hash', hash)
+
+    setSending(false);
+
+    console.log("hash", hash);
   };
 
   return (
     <>
       <CheckContainer>
-        <CheckImage src="/how-to-write-a-check-crop.jpeg" alt="Check" height="335" width="800" />
-        <RecipientInput value={form.to} onChange={(e) => onFieldChange('to', e.target.value)} />
-        <AmountInput value={form.amount} onChange={(e) => onFieldChange('amount', e.target.value)} />
-        <SignatureInput onClick={transfer}>Sign and Send</SignatureInput>
+        <CheckImage
+          src="/how-to-write-a-check-cropped.jpeg"
+          alt="Check"
+          height="335"
+          width="800"
+        />
+        <RecipientInput
+          value={form.to}
+          onChange={(e) => onFieldChange("to", e.target.value)}
+        />
+        <AmountInput
+          value={form.amount}
+          onChange={(e) => onFieldChange("amount", e.target.value)}
+        />
+        <AmountText>
+          {form.amount <= 0 ? "" : converter.toWords(form.amount)}
+        </AmountText>
+        {sending ? (
+          <LoadingOutlined
+            style={{
+              fontSize: 24,
+              position: "absolute",
+              top: "69%",
+              left: "73%",
+            }}
+            spin
+          />
+        ) : (
+          <SignatureInput onClick={transfer}>Sign and Send</SignatureInput>
+        )}
       </CheckContainer>
     </>
   );
@@ -99,32 +129,36 @@ const CheckContainer = styled.div`
 
 const CheckImage = styled.img`
   width: 100%;
-  
 `;
 
 const RecipientInput = styled(Input)`
   position: absolute;
-  top: 38%;
-  left: 23%;
+  top: 35%;
+  left: 17%;
   height: 30px;
   width: 300px;
 `;
 
 const AmountInput = styled(Input)`
   position: absolute;
-  top: 38%;
-  left: 70%;
+  top: 35%;
+  left: 76%;
   height: 30px;
   width: 100px;
 `;
 
 const SignatureInput = styled(Button)`
   position: absolute;
-  top: 58%;
-  left: 54%;
+  top: 69%;
+  left: 59%;
   height: 30px;
   width: 250px;
 `;
 
+const AmountText = styled.p`
+  position: absolute;
+  top: 52%;
+  left: 5%;
+`;
 
 export default TransactionModal;
