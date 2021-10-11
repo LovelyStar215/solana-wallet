f94a538298935b04312457e6a44565ee3c834b08
LovelyStar215 <ericjin106@gmail.com>
2021-10-11 15:27:46 -0500

    Add HEAD to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import type { NextPage } from "next";
+import Head from "next/head";
 import CreateAccount from "../components/CreateAccount";
 import RestoreAccount from "../components/RestoreAccount";
 import styled from "styled-components";
@@ -7,6 +8,13 @@ import styled from "styled-components";
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charSet="utf-8" />
+        <title>Wallet Tutorial</title>
+        <meta name="description" content="Web3 tutorial for Solana crypto wallet." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <HomeTitle>
         A simple, non-custodial crypto wallet for managing{" "}
         <a href="https://solana.com/">Solana</a> digital assets.
