35c5cd8d799ed3d1e0d47ca2bd660edf7ef61aae
LovelyStar215 <ericjin106@gmail.com>
2021-10-16 15:21:13 -0500

    Refactor comments on utils for refreshBalance challenge

--- utils/index.ts
+++ utils/index.ts
@@ -1,31 +1,31 @@
+// Import any additional classes and/or functions needed from Solana's web3.js library:
 import {
-  Connection,
   Cluster,
-  clusterApiUrl,
-  LAMPORTS_PER_SOL,
   Keypair,
 } from "@solana/web3.js";
 
-// *Step 2*: implement a function that gets an account's balance
+// *Step 3*: implement a function that gets an account's balance
 const refreshBalance = async (network: Cluster, account: Keypair | null) => {
   if (!account) return 0;
 
   try {
-    // (a) instantiate a connection using clusterApiUrl with the active network
-    const connection = new Connection(clusterApiUrl(network), "confirmed");
-    console.log(connection);
-    // console.log("Balance functionality not implemented yet!")
-    // const connection = "";
-    // (b) get the key from the active account
-    const publicKey = account.publicKey;
-    // const publicKey = "";
-    // (c) get the account's balance from the connection instance
-    const balance = await connection.getBalance(publicKey);
-    // const balance = 0
-    // (d) return the balance in SOL
-    // return balance / LAMPORTS_PER_SOL;
-    console.log(balance)
-    return balance / LAMPORTS_PER_SOL
+    // (a) review the import guidance on line 1
+    // (b) instantiate a connection using clusterApiUrl with the active network
+    // Documentation References:
+    //   https://solana-labs.github.io/solana-web3.js/classes/Connection.html
+    //   https://solana-labs.github.io/solana-web3.js/modules.html#clusterApiUrl
+    console.log("Balance functionality not implemented yet!")
+    const connection = "";
+
+    // (c) get the key using one of the accesors for active account
+    // Documentation Reference: https://solana-labs.github.io/solana-web3.js/classes/Keypair.html
+    const publicKey = "";
+
+    // (d) get the account's balance using the connection instance
+    // Documentation Reference: https://solana-labs.github.io/solana-web3.js/classes/Connection.html
+    const balance = 0
+
+    return balance
   } catch (error) {
     console.log(error);
     return 0;
